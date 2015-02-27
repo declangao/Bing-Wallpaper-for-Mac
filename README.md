371e1027250568292acd4d827835e995d6c06ffe
Declan Gao <Declan@Tron.local>
2015-02-27 23:43:39 +1300

    New features and bug fixes

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
 # Bing Wallpapaer for Mac
-A simple Python script to download Bing image of the day and set it as wallpaper on your Mac OS X.
+A simple Python script capable of batch-downloading and setting Bing picture of the day as wallpaper on your Mac OS X.
 
 ## About
-Ever since the switch to Mac, I've kinda been missing [PyBingWallpaper](https://github.com/genzj/pybingwallpaper). So I wrote this script to do pretty much the same thing. Microsoft provides JSON API to get the images. This makes things so much easier compared to my initial idea, which was to scrape the webpage. 
+Ever since the switch to Mac, I've kinda been missing [PyBingWallpaper](https://github.com/genzj/pybingwallpaper). So I wrote this script to do pretty much the same thing. 
 
-PS: This is only a Python script. You can convert it to an app with something likee `py2app` if you want. What you do with it is entirely up to you. I'll probably make a native OS X app for this since I've been learning Swift lately.
+Microsoft provides an API to get the images. This makes things so much easier compared to my initial idea, which was to scrape the webpage. 
+
+PS: This is only a Python script. You can convert it to an app with something like `py2app` if you want. What you do with it is entirely up to you. I'll probably make a native OS X app for this since I've been learning Swift lately.
 
 ## Usage
 You can modify the Configurations section to set your own image directory and country code. Wallpapers will be saved to `/Users/USERNAME/Pictures/BingWallpaper` by default.
 
+Download today's Bing picture of the day and set it as wallpaper:
+
+```
+python bing.py
+```
+
+Use ```-d``` or ```--download``` argument to download and save the last 8 pictures without changing the current wallpaper. You might use this option if you want to change wallpapers automatically by adding the wallpaper directory in ```System Preferences```.
+
+```
+python bing.py -d
+```
+
+Use ```-h``` or ```--help``` to display help message.
+
+Note:
 [Requests](http://docs.python-requests.org/en/latest/) was used to simplfy the process. Make sure you have it installed before running the script.
 
 ## License
--- bing.py
+++ bing.py
@@ -11,7 +11,7 @@ IMAGE_DIR = ''
 COUNTRY_CODE = ''
 
 
-import requests, urllib
+import requests, urllib, sys
 from os.path import join, expanduser, isfile, exists
 from os import makedirs
 
@@ -28,7 +28,7 @@ def get_wallpaper_path(file_name):
     if '' != IMAGE_DIR.strip():
         dir = IMAGE_DIR
     else:
-        dir = join(expanduser("~"), 'Pictures/BingWallpaper1')
+        dir = join(expanduser("~"), 'Pictures/BingWallpaper')
 
     if not exists(dir):
         makedirs(dir)
@@ -37,18 +37,19 @@ def get_wallpaper_path(file_name):
     return file_path
 
 
-# Download the image with given URL
-def download_image(url):
+# Download a image with given URL
+def download_image(url, download_only=False):
     file_name = url.split('/')[-1]
     file_path = get_wallpaper_path(file_name)
 
     if isfile(file_path):
-        print('Skipped - Image exists.')
+        print('Skipped - ' + file_name + ' exists already.')
     else:
         urllib.urlretrieve(url, file_path)
         print('Image downloaded --> ' + file_path)
 
-    set_wallpaper(file_path)
+    if not download_only:
+        set_wallpaper(file_path)
 
 
 # Set Finder wallpaper
@@ -60,13 +61,49 @@ def set_wallpaper(file_path):
         print('Wallpaper set to ' + file_path)
 
 
+# Display help message
+def print_help_message():
+    msg = '''
+Bing Wallpaper for Mac version 1.1
+By Declan Gao  http://declangao.me
+
+Bing Wallpaper for Mac can batch download and set Bing image of the day as wallpaper on OS X.
+
+Usage: 
+python bing.py [option]
+
+no argument         download today's picture of the day and set it as wallpaper
+-d or --download    download and save the last 8 pictures withouth changing the current wallpaper
+-h or --help        display this help message
+    '''
+    print(msg)
+    sys.exit()
+
+
 def main():
+    # Check arguments
+    if len(sys.argv) == 1:
+        flag_download_only= False
+    elif len(sys.argv) == 2:
+        if '-d' == sys.argv[1] or '--download' == sys.argv[1]:
+            flag_download_only = True
+        elif '-h' == sys.argv[1] or '--help' == sys.argv[1]:
+            print_help_message()
+        else:
+            print('Invalid argument!')
+            print_help_message()
+    else:
+        print('Invalid arguments!')
+        print_help_message()
+
+    # Choose a proper URL
+    # The API only returns 8 pictures at most. No need to set the number higher than 8.
     # See http://stackoverflow.com/questions/10639914/is-there-a-way-to-get-bings-photo-of-the-day
-    
-    # URL to return 8 images
-    # url = 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8'
-    # URL to return the newest image
-    url = 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
+    if flag_download_only:
+        url = 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8'
+    else:
+        # Set n=8 to get only the newest picture of the day
+        url = 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
 
     # Use country code if provided
     if '' != COUNTRY_CODE.strip():
@@ -76,18 +113,22 @@ def main():
     response = requests.get(url)
     if response.status_code == 200:
         json = response.json() # Get JSON
-        images = json['images']
 
-        # Multiple wallpapers
-        # for i in range(len(images)):
-        #     url = 'http://www.bing.com' + images[i]['url']
-        #     download_image(url)
-
-        # Only the newest one
-        url = 'http://www.bing.com' + images[0]['url']
-        download_image(url)
+        if 'images' in json:
+            images = json['images']
+        else:
+            sys.exit('JSON error. Please try again later...')
+        
+        # Start downloading!
+        print('Downloading...')
+        for i in range(len(images)):
+            url = 'http://www.bing.com' + images[i]['url']
+            if flag_download_only:
+                download_image(url, True)
+            else:
+                download_image(url)
     else:
-        print('Oops, something went wrong...')
+        print('Cannot reach server. Please check your internet connection...')
 
 
 if __name__ == '__main__':
